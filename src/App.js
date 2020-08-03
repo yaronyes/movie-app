6cb35ef51084bd025c33c35693680ae430a6388d
yaronyes <yaronyes@hotmail.com>
2020-08-03 21:39:43 +0300

    reading data from json file

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import './App.css';
 import { Container } from 'react-bootstrap';
 import ActorModel from './data-model/ActorModel';
 import ActorsView from './components/ActorsView';
+import data from './data/actors.json';
 
 
 function App() {
-  const actors = [];
-  actors.push(new ActorModel(1, "Jack", "Nicholson", "1937-04-22", "https://m.media-amazon.com/images/M/MV5BMTQ3OTY0ODk0M15BMl5BanBnXkFtZTYwNzE4Njc4._V1_.jpg", "https://www.imdb.com/name/nm0000197"));
-  actors.push(new ActorModel(2, "Robert", "De Niro", "1943-08-17", "https://m.media-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_.jpg", "https://www.imdb.com/name/nm0000134"));
-  actors.push(new ActorModel(3, "Al", "Pacino", "1940-04-25", "https://m.media-amazon.com/images/M/MV5BMTQzMzg1ODAyNl5BMl5BanBnXkFtZTYwMjAxODQ1._V1_.jpg", "https://www.imdb.com/name/nm0000199"));
+  // const actors = [];
+  // actors.push(new ActorModel(1, "Jack", "Nicholson", "1937-04-22", "https://m.media-amazon.com/images/M/MV5BMTQ3OTY0ODk0M15BMl5BanBnXkFtZTYwNzE4Njc4._V1_.jpg", "https://www.imdb.com/name/nm0000197"));
+  // actors.push(new ActorModel(2, "Robert", "De Niro", "1943-08-17", "https://m.media-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_.jpg", "https://www.imdb.com/name/nm0000134"));
+  // actors.push(new ActorModel(3, "Al", "Pacino", "1940-04-25", "https://m.media-amazon.com/images/M/MV5BMTQzMzg1ODAyNl5BMl5BanBnXkFtZTYwMjAxODQ1._V1_.jpg", "https://www.imdb.com/name/nm0000199"));
+
+  const actors = data.map(actor => new ActorModel(actor.id, actor.firstName, actor.lastName, actor.birthday, actor.imageUrl, actor.imdbLink));
 
   return (
     <Container>
