f62e515cf1b649f7004c67b3b67f194744d28a4f
yaronyes <yaronyes@hotmail.com>
2020-08-07 17:21:49 +0300

    changing the cursor when fetching data

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,6 +7,7 @@ import './Movie.css';
 // movie - MovieModel class
 // State:
 // extraData - object with extras movie data to gain upon opening the accordion
+// cursor - changing the cursor when fetching data
 const Movie = (props) => {
     const { movie } = props;    
     const [extraData, setExtraData] = useState({
@@ -14,21 +15,26 @@ const Movie = (props) => {
         mainStars: [],
         length: 0
     });
+    const [cursor, setCursor] = useState("pointer-cursor")
 
     // populate the movie with extra data upon opening the accordion
     const getMovieData = async () => {        
+        setCursor("wait-curser");
+        
         await movie.populate();
 
         setExtraData({
             director:  <a href={`https://www.imdb.com/name/${movie.director.imdb_id}`} rel="noopener noreferrer" target="_blank">{movie.director.name}</a>,
             mainStars: movie.mainStars.map((actor, i) => <span key={i}><a href={`https://www.imdb.com/name/${actor.imdb_id}`} rel="noopener noreferrer" target="_blank">{actor.name}</a>, </span>),
             length: movie.length
-        })
+        });
+
+        setCursor("pointer-cursor");
     }
 
     return (
         <div className="comp-movie">            
-            <Card onClick={getMovieData}>
+            <Card onClick={getMovieData} className={cursor}>
                 <Accordion.Toggle as={Card.Header} eventKey={movie.id}>
                 {movie.name}
                 </Accordion.Toggle>
